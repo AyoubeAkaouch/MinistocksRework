2de2e1c2cdbcd090fc08a04bed8573451b618311
aghiles <aghiles_1@hotmail.com>
2018-03-03 14:26:27 -0500

    added new xml layout for larger widget Issue #9

---
+++ src/main/java/nitezh/ministock/activities/configure/ConfigureActivity3x4.java
@@ -0,0 +1,8 @@
+package nitezh.ministock.activities.configure;
+
+/**
+ * Created by saito on 3/3/18.
+ */
+
+public class ConfigureActivity3x4 {
+}
---
+++ src/main/java/nitezh/ministock/activities/widget/WidgetProvider3x4.java
@@ -0,0 +1,8 @@
+package nitezh.ministock.activities.widget;
+
+/**
+ * Created by saito on 3/3/18.
+ */
+
+public class WidgetProvider3x4 {
+}
