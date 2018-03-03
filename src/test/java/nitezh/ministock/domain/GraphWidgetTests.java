bece4ca794b29247b4ec5e4625cbf3e4c045c206
Mohamed Lemine <mohamedlemineelhadj@outlook.com>
2018-03-03 10:44:18 -0500

    Test correct preferences are removed from 4x4 graph widget. Issue #19

--- src/test/java/nitezh/ministock/domain/GraphWidgetTests.java
+++ src/test/java/nitezh/ministock/domain/GraphWidgetTests.java
@@ -1,28 +1,42 @@
 package nitezh.ministock.domain;
 
+import android.content.SharedPreferences;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 
 import nitezh.ministock.Storage;
+import nitezh.ministock.activities.PreferencesActivity;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 
-/**
- * Created by Giovanni on 2018-03-02.
- */
 @RunWith(RobolectricTestRunner.class)
 public class GraphWidgetTests {
 
+    private PreferencesActivity preferencesActivity;
+    private SharedPreferences sharedPreferences;
+    private PreferenceScreen screen;
 
     private Widget widget;
 
     @Before
     public void setUp() {
+
+        //Setup activity to set the widget size to 4
+        preferencesActivity = Robolectric.setupActivity(PreferencesActivity.class);
+
+        //Set the widget size to 4
+        screen = preferencesActivity.getPreferenceScreen();
+        sharedPreferences = screen.getSharedPreferences();
+        sharedPreferences.edit().putInt("widgetSize",4).commit();
 
         int WIDGET_ID = 1;
         int WIDGET_SIZE = 4; //select graph widget
@@ -56,4 +70,22 @@ public class GraphWidgetTests {
         System.out.println("Current stock is now: "+widget.getStock(0));
         assertEquals(widget.getStock(0),"GOOGL");
     }
+
+    @Test
+    public void TestIfUnneededPreferencesAreGone(){
+        //Re-setup the activity with new widget size (4)
+        preferencesActivity = Robolectric.setupActivity(PreferencesActivity.class);
+
+        PreferenceScreen advanced = (PreferenceScreen) preferencesActivity.findPreference("advanced");
+        Preference portfolio = preferencesActivity.findPreference("portfolio");
+        Preference update_now = preferencesActivity.findPreference("update_now");
+
+        //Assert
+        assertNull(advanced);
+        assertNull(portfolio);
+        assertNull(update_now);
+
+    }
+
+
 }
