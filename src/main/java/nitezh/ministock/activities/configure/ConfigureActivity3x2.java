0c0f707f749d94706233787698508c41cd9ac369
pcabro <i_rozyba@encs.concordia.ca>
2018-03-04 12:24:53 -0500

    modified xml layout for 15 stocks

---
+++ src/main/java/nitezh/ministock/activities/configure/ConfigureActivity3x2.java
@@ -0,0 +1,8 @@
+package nitezh.ministock.activities.configure;
+
+/**
+ * Created by pcabr on 2018-03-04.
+ */
+
+public class ConfigureActivity3x2 {
+}
---
+++ src/main/java/nitezh/ministock/activities/widget/WidgetProvider3x2.java
@@ -0,0 +1,8 @@
+package nitezh.ministock.activities.widget;
+
+/**
+ * Created by pcabr on 2018-03-03.
+ */
+
+public class WidgetProvider3x2 {
+}
