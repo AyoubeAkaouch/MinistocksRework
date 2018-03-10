4e4fe4e21ed636805f1b3c9b6a5374b7dbbc2c9f
gprattico <gprattico95@gmail.com>
2018-03-10 18:04:40 -0500

    Add Broadcast class to receive intents and initiate update requests. #23

---
+++ src/main/java/nitezh/ministock/MyBroadcastReceiver.java
@@ -0,0 +1,29 @@
+package nitezh.ministock;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import nitezh.ministock.activities.widget.WidgetProviderBase;
+
+/**
+ * Created by Giovanni on 2018-03-10.
+ */
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Storage prefs = PreferenceStorage.getInstance(context);
+
+        if (intent.getAction().equals(Intent.ACTION_USER_PRESENT) && prefs.getBoolean("unlock_after_refresh",false)) {
+            //update the widget
+            WidgetProviderBase.updateWidgets(context, WidgetProviderBase.UpdateType.VIEW_UPDATE);
+
+        }
+
+    }
+
+}
+
