da11f4410c70cc600cd64e99382727c94af792e9
gprattico <gprattico95@gmail.com>
2018-03-14 18:43:02 -0400

    Fix bug where widget would still update if user deselected update on unlock. Add test class for broadcaster class. #23

--- src/main/java/nitezh/ministock/MyBroadcastReceiver.java
+++ src/main/java/nitezh/ministock/MyBroadcastReceiver.java
@@ -3,6 +3,7 @@ package nitezh.ministock;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.storage.StorageManager;
 import android.util.Log;
 
 import nitezh.ministock.activities.widget.WidgetProviderBase;
@@ -18,11 +19,9 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
 
         Storage prefs = PreferenceStorage.getInstance(context);
 
-        if (intent.getAction().equals(Intent.ACTION_USER_PRESENT) && prefs.getBoolean("unlock_after_refresh",true)) {
-
+        if (intent.getAction().equals(Intent.ACTION_USER_PRESENT) && prefs.getBoolean("unlock_after_refresh",false)) {
             //update the widget
             WidgetProviderBase.updateWidgets(context, WidgetProviderBase.UpdateType.VIEW_UPDATE);
-
         }
 
     }
