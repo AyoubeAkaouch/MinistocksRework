13776e8520e2c22f6dc8d6c02983b00d2b3ece48
AyoubeAkaouch <ayoube.akaouch@gmail.com>
2018-04-11 19:08:12 -0400

    Added UI test for adding a stock. - Also fixed the footer UI test, to give it time to update the footer before checking if it was removed. - Issue #67

--- src/androidTest/java/nitezh/ministock/UITests.java
+++ src/androidTest/java/nitezh/ministock/UITests.java
@@ -4,10 +4,14 @@ package nitezh.ministock;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.SdkSuppress;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.SearchCondition;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObject2;
 import android.support.test.uiautomator.UiObjectNotFoundException;
 import android.support.test.uiautomator.UiSelector;
+import android.support.test.uiautomator.Until;
 
 
 import org.junit.Before;
@@ -22,7 +26,10 @@ import static junit.framework.Assert.assertTrue;
 public class UITests {
 
     private UiDevice mDevice;
-
+    //*************************
+    //UI tests have to be run on a 1080p screen
+    //If not the widget will be stuck on loading data...
+    //*************************
     @Before
     public void setUp() throws UiObjectNotFoundException {
         mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
@@ -32,7 +39,7 @@ public class UITests {
 
     //This test assumes that footer is visible and widget is on the home screen
     @Test
-    public void testRemoveFooterOption() throws UiObjectNotFoundException{
+    public void testRemoveFooterOption() throws UiObjectNotFoundException, InterruptedException{
 
         UiObject footer = mDevice.findObject(new UiSelector().className("android.widget.TextView")
                 .resourceId("nitezh.ministock:id/text5"));
@@ -50,7 +57,7 @@ public class UITests {
         UiObject appearance = mDevice.findObject(new UiSelector().className("android.widget.LinearLayout").index(1));
         appearance.clickAndWaitForNewWindow();
 
-        UiObject footerDisplay = mDevice.findObject(new UiSelector().className("android.widget.LinearLayout").index(3));
+        UiObject footerDisplay = mDevice.findObject(new UiSelector().className("android.widget.LinearLayout").index(4));
         footerDisplay.clickAndWaitForNewWindow();
 
         //Remove footer
@@ -61,6 +68,7 @@ public class UITests {
         mDevice.pressHome();
 
         //footer should be gone
+        Thread.sleep(3000);
         assertFalse(footer.exists());
 
         //Set footer back to visible
@@ -73,6 +81,45 @@ public class UITests {
                 .text("Visible"));
         visibleFooter.click();
         mDevice.pressHome();
+    }
+
+    //The widget has to be setup with the default values
+    @Test
+    public void addStockToWidget() throws UiObjectNotFoundException,InterruptedException {
+
+        //Verify that no stock has been setup in that section
+        UiObject stockRow = mDevice.findObject(new UiSelector().className("android.widget.TextView")
+                .resourceId("nitezh.ministock:id/text21"));
+        assertTrue(stockRow.getText().equals(""));
+
+        //Setup the left click on options
+        UiObject widgetLeft = mDevice.findObject(new UiSelector().className("android.widget.LinearLayout")
+                .resourceIdMatches("nitezh.ministock:id/widget_left"));
+        widgetLeft.clickAndWaitForNewWindow();
+
+        UiObject advanced = mDevice.findObject(new UiSelector().className("android.widget.LinearLayout").index(1));
+        advanced.clickAndWaitForNewWindow();
+
+        UiObject stockSlot3 = mDevice.findObject(new UiSelector().className("android.widget.LinearLayout").index(1));
+        stockSlot3.clickAndWaitForNewWindow(1000);
+
+        UiObject stockSearch = mDevice.findObject(new UiSelector().className("android.widget.EditText").resourceId("android:id/search_src_text"));
+        Thread.sleep(2000);
+
+        stockSearch.setText("amzn");
+        Thread.sleep(5000);
+
+        mDevice.click(514,271);
+        Thread.sleep(2000);
+
+        mDevice.pressHome();
+        widgetLeft.clickAndWaitForNewWindow();
+        mDevice.pressHome();
+
+        Thread.sleep(10000);
+
+        //Check if row has been set with a stock
+        assertTrue(!stockRow.getText().equals(""));
     }
 
 }
